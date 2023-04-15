3f46a5e2fb0e8abb91ea3f515536698a5a890e60
noceive <nocivescience@gmail.com>
2023-04-15 00:08:09 -0400

    primer commit

--- bar.py
+++ bar.py
@@ -1,4 +1,5 @@
 from manim import *
+import itertools as it
 class Bar(Scene):
     CONFIG={
         'line_config':{
@@ -10,18 +11,20 @@ class Bar(Scene):
         line_number=NumberLine(**self.CONFIG['line_config'])
         bar=self.get_bar(line_number)
         value_tracker=ValueTracker(1)
-        values=[1,2,3,4,5,6,7,8,9,10]
-        # bar.add_updater(lambda b:self.set_bar(b,line_number,value_tracker.get_value()))
-        self.add(line_number,bar)
+        indice=self.get_data()
+        self.add(line_number,bar,indice)
         bar.add_updater(lambda b:b.stretch_to_fit_width(value_tracker.get_value()*line_number.x_range[-1],about_edge=LEFT))
-        self.play(value_tracker.animate.set_value(10),run_time=10)
-        # for value in values:
-        #     anims=[ApplyMethod(bar.stretch_to_fit_width,value*line_number.x_range[-1],about_edge=LEFT)]
-        #     self.play(*anims)
+        indice[1].add_updater(lambda i:i.set_value(value_tracker.get_value()))
+        values=np.array([self.get_randon_value() for i in range(10)])
+        np.random.shuffle(values)
+        value_cycle=list(it.islice(it.cycle(values),0,200))
+        np.random.shuffle(value_cycle)
+        for value in value_cycle:
+            self.play(ApplyMethod(value_tracker.set_value,value),run_time=.1)
         self.wait()
     def get_bar(self,line):
         bar=Rectangle(height=.3,width=line.x_range[-1]*2)
-        bar.move_to(line.number_to_point(0),aligned_edge=LEFT)
+        bar.move_to(line.number_to_point(0),aligned_edge=LEFT) 
         return bar
     def set_bar(self,bar,line,value):
         bar.set_width(line.number_to_point(value))
@@ -31,3 +34,14 @@ class Bar(Scene):
             about_edge=LEFT
         )
         return bar
+    def get_randon_value(self):
+        return np.random.randint(1,10)
+    def get_data(self):
+        indice=VGroup(
+            Tex('Indice'),
+            Integer(0)
+        )
+        indice.align_to(indice[0],DOWN)
+        indice.arrange(RIGHT)
+        indice.move_to(UP*2)
+        return indice
